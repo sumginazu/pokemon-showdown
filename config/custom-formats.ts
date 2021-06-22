42316e9278368b654ddf6f6bb60acd3bb9a80cf4
alex shoup <shoutwithap@gmail.com>
2021-06-21 23:27:47 -0700

    add custom format file

---
+++ config/custom-formats.ts
@@ -0,0 +1,24 @@
+// Note: This is the list of formats
+// The rules that formats use are stored in data/rulesets.ts
+
+export const Formats: FormatList = [
+	// ROY LEAGUE BABEY
+	///////////////////////////////////////////////////////////////////
+
+	{
+		section: "ROY LEAGUE!!!!!!!!!!",
+		column: 1,
+	},
+	{
+		name: "[Gen 8] Roy Monotype (Nat Dex) 2020",
+		mod: 'gen8',
+		ruleset: ['Standard NatDex', 'Roy Said So Clause', 'Same Type Clause +Mega', 'Item Clause... 2!', 'Evasion Moves Clause', 'Dynamax Clause', 'Sleep Clause Mod', 'Forced Mega Evolution Mod'],
+		banlist: [
+			'Arceus', 'Blacephalon', 'Blaziken-Mega', 'Buzzwole', 'Calyrex-Ice', 'Calyrex-Shadow', 'Celesteela', 'Darkrai', 'Deoxys-Base', 'Deoxys-Attack', 'Dialga', 'Dracovish', 'Eternatus', 'Genesect',
+			'Gengar-Mega', 'Giratina', 'Giratina-Origin', 'Groudon', 'Guzzlord', 'Ho-Oh', 'Hoopa-Unbound', 'Kartana', 'Kyogre', 'Kyurem-Black', 'Kyurem-White', 'Lucario-Mega', 'Lugia',
+			'Lunala', 'Magearna', 'Marshadow', 'Mawile-Mega', 'Medicham-Mega', 'Metagross-Mega', 'Mewtwo', 'Naganadel', 'Necrozma-Dawn-Wings', 'Necrozma-Dusk-Mane', 'Nihilego', 'Palkia', 'Pheromosa', 'Poipole',
+			'Rayquaza', 'Reshiram', 'Salamence-Mega', 'Shaymin-Sky', 'Solgaleo', 'Stakataka', 'Xerneas', 'Xurkitree', 'Yveltal', 'Zacian', 'Zacian-Crowned', 'Zamazenta', 'Zamazenta-Crowned', 'Zekrom', 'Zygarde-Base',
+			'Arena Trap', 'Battle Bond', 'Moody', 'Power Construct', 'Shadow Tag', 'Baton Pass',
+		],
+	},
+];
